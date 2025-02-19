56bd66a8b08b37d7c9fe37024ca443b6e690aef7
Anushri2831 <anushrichinchankar5@gmail.com>
2025-02-19 10:50:20 +0530

    re-aligned the entire contents of popular experiences

--- my-react-app/src/component/PopularExperiences.jsx
+++ my-react-app/src/component/PopularExperiences.jsx
@@ -20,13 +20,13 @@ const PopularExperiences = () => {
   return (
     <div className="popular-experiences">
       <h2>Popular experiences</h2>
-      <div className="experiences-list">
+      <div className="experiences-list" >
         {experiences.map((exp) => (
           <div key={exp.id} className="experience-card">
             <img src={exp.image} alt={exp.title} className="experience-img" />
             <div className="experience-details">
-              <h3>{exp.title}</h3>
-              <p>{exp.description}</p>
+              <h3 >{exp.title}</h3>
+              <p >{exp.description}</p>
             </div>
           </div>
         ))}
